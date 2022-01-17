251f0d82b3a1342c71827aa23b16961e0d375d1e
limchaekyung <corud505@naver.com>
2022-01-17 23:50:03 +0900

    style, data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,30 +14,48 @@ const GET_MOVIES = gql`
 `
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
 
-const Header = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
 
 const Title = styled.h1`
-    font-size: 35px;
-`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
 const SubTitle = styled.h3`
-    font-size: 35px;
-`
+  font-size: 35px;
+`;
 
 const Loading = styled.div`
-    font-size: 18px;
-    opacity: 0.5;
-    font-weight: 500;
-    margin-top: 10px;
-`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
 
 export default () => {
     const {loading, data} = useQuery(GET_MOVIES);
@@ -48,9 +66,13 @@ export default () => {
                 <SubTitle>I Love GraphQL</SubTitle>
             </Header>
             {loading && <Loading>Loading...</Loading>}
-            {!loading && 
-                data.movies && 
-                data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+            {!loading && data.movies && (
+              <Movies>
+                {data.movies.map(m => (
+                  <Movie key={m.id} id={m.id} bg={m.medium_cover_image}/>
+                ))}
+                </Movies>
+            )}
         </Container>
     )
 }
